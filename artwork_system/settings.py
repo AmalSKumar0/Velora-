62de00229025f5ea764e2e4df24886599f1403fc
AmalSKumar0 <amalskumarofficialz@gmail.com>
2026-05-01 18:26:09 +0530

    settings update for deployment

--- artwork_system/settings.py
+++ artwork_system/settings.py
@@ -13,7 +13,7 @@ DEBUG = os.getenv("DEBUG")
 RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID")
 RAZORPAY_KEY_SECRET = os.getenv("RAZORPAY_KEY_SECRET")
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["velora.amalskumar.dev"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
